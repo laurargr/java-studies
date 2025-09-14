44074a4af170769166c6f16f8c528e778e1e243f
Laura Ribeiro <lauragribeiro9@gmail.com>
2025-09-14 17:38:52 +0300

    exc 3

---
+++ Tabuada.java
@@ -0,0 +1,18 @@
+public class Tabuada {
+    public void tabuadaOf1() {
+        for (int i = 0; i < 20; i++){
+            int a = 1;
+            int b = i + 1;
+            int c = a * b; 
+            System.out.println("1 * " + b + " = " + c);
+        }
+    }
+    public static void tabuadaOf7() {
+        for (int i = 0; i < 20; i++){
+            int a = 7;
+            int b = i + 1;
+            int c = a * b;
+            System.out.println( "7 * " + b + " = " + c);
+        }
+    }
+}
