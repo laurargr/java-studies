ee36ef6adb70a5ddf4099094b1f31c9458a3cedf
Laura Ribeiro <lauragribeiro9@gmail.com>
2025-09-18 22:07:10 +0300

    feat: add better User

---
+++ Day4/UserNew.java
@@ -0,0 +1,41 @@
+package Day4;
+
+public class UserNew {
+    public String name;
+    public int age;
+    public String gender;
+
+    public UserNew(String name, int age, String gender) {
+        this.age = age;
+        this.name = name;
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void getInfo() {
+        System.out.println("Name: " + name + " Age: " + age + " Gender: " + gender );
+    }
+}
