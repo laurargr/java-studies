9ebe87330d52b9388b65cf1a888c4554f6a14dad
Laura Ribeiro <lauragribeiro9@gmail.com>
2025-09-18 21:39:05 +0300

    exc 26

---
+++ Day5/Exc26/GymMembership.java
@@ -0,0 +1,39 @@
+package Day5.Exc26;
+
+public class GymMembership {
+    protected String name;
+    protected String type;
+    protected int duration;
+    protected int price;
+
+    public GymMembership(String name, int duration) {
+        this.name = name;
+        this.duration = duration;
+        this.type = "normal mermbership";
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
+    public void membershipFee (){
+        price = 26 * duration;
+        System.out.println(price);
+    }
+}
---
+++ Day5/Exc26/PremiumMembership.java
@@ -0,0 +1,15 @@
+package Day5.Exc26;
+
+import Day5.Exc26.GymMembership;
+
+public class PremiumMembership extends GymMembership {
+    public PremiumMembership(String name, int duration) {
+        super(name, duration);
+        this.type = "Premium membership";
+    }
+    @Override
+    public void membershipFee () {
+        this.price = 39 * this.duration;
+        System.out.println(price);
+    }
+}
