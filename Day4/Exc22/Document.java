986acc1b20e52be1f0d2418d7dadcdfd660fbf9e
Laura Ribeiro <lauragribeiro9@gmail.com>
2025-09-18 23:55:31 +0300

    code identation

--- Day4/Exc22/Document.java
+++ Day4/Exc22/Document.java
@@ -1,10 +1,12 @@
 package Day4.Exc22;
+
 import Day4.Exc22.Searchable;
 
 public class Document extends Searchable {
-    public Document () {
+    public Document() {
         super();
     }
+
     @Override
     public void search(String keyword, String path) {
 
--- Test.java
+++ Test.java
@@ -1,194 +1,234 @@
-import Day4.Exc24.*;
+// import Day5.Exc25.*;
+// import Day5.Exc26.GymMembership;
+// import Day5.Exc26.PremiumMembership;
+
+import java.util.Scanner;
 
 public class Test {
-    public static void main(String[] args) {
-        // Tabuada tab = new Tabuada();
-        // tab.tabuadaOf1();
-        // Tabuada.tabuadaOf7(); 
+  public static void main(String[] args) {
+    // Tabuada tab = new Tabuada();
+    // tab.tabuadaOf1();
+    // Tabuada.tabuadaOf7();
 
-        // IAmSimple test = new IAmSimple();
-        // System.out.println(test.testingReturn());
+    // IAmSimple test = new IAmSimple();
+    // System.out.println(test.testingReturn());
 
-        // Operations op = new Operations();
-        // System.out.println(op.findTheLargestNumber(90, 2));
+    // Operations op = new Operations();
+    // System.out.println(op.findTheLargestNumber(90, 2));
 
-        // Calculator calc = new Calculator();
-        // System.out.println(calc.sum(2, 2));
-        // System.out.println(calc.sub(2, 2));
-        // System.out.println(calc.mult(5, 5));
-        // System.out.println(calc.div(25, 5));
+    // Calculator calc = new Calculator();
+    // System.out.println(calc.sum(2, 2));
+    // System.out.println(calc.sub(2, 2));
+    // System.out.println(calc.mult(5, 5));
+    // System.out.println(calc.div(25, 5));
 
-        // ImprovedCalculator calc = new ImprovedCalculator();
-        // System.out.println(calc.calculate(2, '+', 3));
-        // System.out.println(calc.calculate(2, '-', 3));
-        // System.out.println(calc.calculate(2, '*', 3));
-        // System.out.println(calc.calculate(3, '/', 3));
+    // ImprovedCalculator calc = new ImprovedCalculator();
+    // System.out.println(calc.calculate(2, '+', 3));
+    // System.out.println(calc.calculate(2, '-', 3));
+    // System.out.println(calc.calculate(2, '*', 3));
+    // System.out.println(calc.calculate(3, '/', 3));
 
-        // Smart fib = new Smart();
-        // fib.fibonacci(90);
+    // Smart fib = new Smart();
+    // fib.fibonacci(90);
 
-        // Smart2 fac = new Smart2();
-        // fac.factorialWithFor(5);
-        // fac.factorialWithWhile(5);
-        // fac.factorialWithDoWhile(1);
+    // Smart2 fac = new Smart2();
+    // fac.factorialWithFor(5);
+    // fac.factorialWithWhile(5);
+    // fac.factorialWithDoWhile(1);
 
-        // Animal cachorro = new Animal("hope", 2);
-        // System.out.println(cachorro.getName());
-        // cachorro.setName("laura");
-        // System.out.println(cachorro.getName());
+    // Animal cachorro = new Animal("hope", 2);
+    // System.out.println(cachorro.getName());
+    // cachorro.setName("laura");
+    // System.out.println(cachorro.getName());
 
-        // Animal novocachorro = new Animal(String("hope", 2, "brown");
-        // System.out.println(novocachorro.t));
+    // Animal novocachorro = new Animal(String("hope", 2, "brown");
+    // System.out.println(novocachorro.t));
 
-        // Animal [] meusAnimais = new Animal[5];
-        // Animal cachorro = new Animal("hope", 2);
-        // Animal gato = new Animal("sasha", 7);
+    // Animal [] meusAnimais = new Animal[5];
+    // Animal cachorro = new Animal("hope", 2);
+    // Animal gato = new Animal("sasha", 7);
 
-        // meusAnimais[0] = cachorro;
-        // meusAnimais[1] = gato;
-        // System.out.println(meusAnimais[0]);
-        // System.out.println(meusAnimais[1]);
+    // meusAnimais[0] = cachorro;
+    // meusAnimais[1] = gato;
+    // System.out.println(meusAnimais[0]);
+    // System.out.println(meusAnimais[1]);
         
-        // Cat cat = new Cat();
-        // System.out.println(cat.name);
-        // System.out.println(cat.age);
-        // Dog hope = new Dog("hope", "bown");
-        // System.out.println(hope.name);
-        // System.out.println(hope.color);
+    // Cat cat = new Cat();
+    // System.out.println(cat.name);
+    // System.out.println(cat.age);
+    // Dog hope = new Dog("hope", "bown");
+    // System.out.println(hope.name);
+    // System.out.println(hope.color);
 
-        // Book a = new Book();
-        // Book b = new Book("null", "nuuu");
-        // Book c = new Book("null", "nuuuu", 40);
+    // Book a = new Book();
+    // Book b = new Book("null", "nuuu");
+    // Book c = new Book("null", "nuuuu", 40);
 
-        // System.out.println(a.title);
-        // System.out.println(b.outhor);
-        // System.out.println(c.price);
+    // System.out.println(a.title);
+    // System.out.println(b.outhor);
+    // System.out.println(c.price);
 
-        // Student lala = new Student(333, "lala", 10);
-        // System.out.println(lala.studentName);
-        // System.out.println(lala.studentId);
-        // System.out.println(lala.grade);
+    // Student lala = new Student(333, "lala", 10);
+    // System.out.println(lala.studentName);
+    // System.out.println(lala.studentId);
+    // System.out.println(lala.grade);
 
-        // System.out.println(ComplexInicialization.x);
-        // System.out.println(ComplexInicialization.y);
-        // System.out.println(ComplexInicialization.z);
+    // System.out.println(ComplexInicialization.x);
+    // System.out.println(ComplexInicialization.y);
+    // System.out.println(ComplexInicialization.z);
 
-//         Rectangle rec = new Rectangle(10, 12);
-//         Rectangle rec2 = new Rectangle(rec);
-//         System.out.println(rec.length);
-//         System.out.println(rec2.width);
+    // Rectangle rec = new Rectangle(10, 12);
+    // Rectangle rec2 = new Rectangle(rec);
+    // System.out.println(rec.length);
+    // System.out.println(rec2.width);
 
-        // Account ac = new Account(-1, 2);
-        // Account ab = new Account(2, 2);
-        // System.out.println(ac.accountNumber);
-        // System.out.println(ab.accountNumber);
+    // Account ac = new Account(-1, 2);
+    // Account ab = new Account(2, 2);
+    // System.out.println(ac.accountNumber);
+    // System.out.println(ab.accountNumber);
 
-//        Point p = new Point(11.2, 0);
-//        Point p2 =  new Point(3, 2);
-//        System.out.println(p.x);
-//        System.out.println(p.y);
-//        System.out.println(p2.x);
-//        System.out.println(p2.y);
+    // Point p = new Point(11.2, 0);
+    // Point p2 =  new Point(3, 2);
+    // System.out.println(p.x);
+    // System.out.println(p.y);
+    // System.out.println(p2.x);
+    // System.out.println(p2.y);
 
-//        String [] addName = new String[10];
-//        for (int i = 0; i < 10; i++) {
-//            addName[i] = "alice" + (i + 1);
-//        }
-//        Classroom classB = new Classroom("8avo B", addName);
-//        System.out.println(classB.className);
-//        System.out.println(Arrays.toString(classB.students));
+    // String [] addName = new String[10];
+    // for (int i = 0; i < 10; i++) {
+    //   addName[i] = "alice" + (i + 1);
+    // }
+    // Classroom classB = new Classroom("8avo B", addName);
+    // System.out.println(classB.className);
+    // System.out.println(Arrays.toString(classB.students));
 
-//        Person p = new Person("laura", 26, "Brazil");
-//        System.out.println(p.getName());
-//        System.out.println(p.getAge());
-//        System.out.println(p.getCountry());
+    // Person p = new Person("laura", 26, "Brazil");
+    // System.out.println(p.getName());
+    // System.out.println(p.getAge());
+    // System.out.println(p.getCountry());
 
-//        BankAccount setN = new BankAccount(22252, 2557415);
-//        System.out.println(setN.getAccountNumber());
-//        System.out.println(setN.getBalance());
+    // BankAccount setN = new BankAccount(22252, 2557415);
+    // System.out.println(setN.getAccountNumber());
+    // System.out.println(setN.getBalance());
 
-//        BankAccount setN = new BankAccount();
-//        BankAccount setB = new BankAccount();
-//        setN.setAccountNumber(5245547);
-//        setB.setBalance(852741);
-//        System.out.println(setN.getAccountNumber());
-//        System.out.println(setB.getBalance());
+    // BankAccount setN = new BankAccount();
+    // BankAccount setB = new BankAccount();
+    // setN.setAccountNumber(5245547);
+    // setB.setBalance(852741);
+    // System.out.println(setN.getAccountNumber());
+    // System.out.println(setB.getBalance());
 
-//        Student laura =  new Student();
-//        laura.setName("laura");
-//        laura.setId(333);
-//        System.out.println(laura.info());
-//        laura.addGrade(9);
-//        System.out.println(laura.info());
-//        laura.addGrade(8);
-//        System.out.println(laura.info());
-//        laura.addGrade(10);
-//        System.out.println(laura.info());
-//        laura.addGrade(8);
-//        System.out.println(laura.info());
-//        laura.addGrade(9);
-//        System.out.println(laura.info());
-//        laura.addGrade(10);
+    // Student laura =  new Student();
+    // laura.setName("laura");
+    // laura.setId(333);
+    // System.out.println(laura.info());
+    // laura.addGrade(9);
+    // System.out.println(laura.info());
+    // laura.addGrade(8);
+    // System.out.println(laura.info());
+    // laura.addGrade(10);
+    // System.out.println(laura.info());
+    // laura.addGrade(8);
+    // System.out.println(laura.info());
+    // laura.addGrade(9);
+    // System.out.println(laura.info());
+    // laura.addGrade(10);
 
-//        Book bible = new Book();
-//        bible.setTitle("bible");
-//        bible.setAuthor("unknown");
-//        bible.setPrice(120);
-//        bible.applyDiscont(22);
-//        System.out.println(bible.getPrice());
+    // Book bible = new Book();
+    // bible.setTitle("bible");
+    // bible.setAuthor("unknown");
+    // bible.setPrice(120);
+    // bible.applyDiscont(22);
+    // System.out.println(bible.getPrice());
 
-//        Account seb = new Account();
-//        seb.setbalance(22);
-//        seb.setAccountNumber(14962);
-//        seb.setHolder("laura");
-//        System.out.println(seb.getbalance());
-//        seb.deposit(22);
-//        System.out.println(seb.getbalance());
-//        seb.withdraw(22);
-//        System.out.println(seb.getbalance());
+    // Account seb = new Account();
+    // seb.setbalance(22);
+    // seb.setAccountNumber(14962);
+    // seb.setHolder("laura");
+    // System.out.println(seb.getbalance());
+    // seb.deposit(22);
+    // System.out.println(seb.getbalance());
+    // seb.withdraw(22);
+    // System.out.println(seb.getbalance());
 
-//    Product phone = new Product();
-//    phone.setName("Phone");
-//    phone.setcode(125);
-//    phone.setPrice(1250);
-//    phone.applyDiscount(21);
-//    System.out.println(phone.getPrice());
+    // Product phone = new Product();
+    // phone.setName("Phone");
+    // phone.setcode(125);
+    // phone.setPrice(1250);
+    // phone.applyDiscount(21);
+    // System.out.println(phone.getPrice());
 
-//        Vehicle test = new Vehicle();
-//        test.speedUp(90);
-//        Vehicle vehicleCar = new Car();
-//        vehicleCar.speedUp(90);
-//        Vehicle vehicleBike = new Bicycle();
-//        vehicleBike.speedUp(90);
-//        Sport basketball = new Basketball();
-//        basketball.play();
-//        Sport football = new Football();
-//        football.play();
-//        Sport rugby = new Rugby();
-//        rugby.play();
+    // Vehicle test = new Vehicle();
+    // test.speedUp(90);
+    // Vehicle vehicleCar = new Car();
+    // vehicleCar.speedUp(90);
+    // Vehicle vehicleBike = new Bicycle();
+    // vehicleBike.speedUp(90);
+    // Sport basketball = new Basketball();
+    // basketball.play();
+    // Sport football = new Football();
+    // football.play();
+    // Sport rugby = new Rugby();
+    // rugby.play();
 
-//        BankAccount saves =  new ChekingAccount();
-//       saves.deposit(15);
-//       saves.showBalance();
-//       saves.withdraw(2);
-//       saves.showBalance();
+    // BankAccount saves =  new ChekingAccount();
+    // saves.deposit(15);
+    // saves.showBalance();
+    // saves.withdraw(2);
+    // saves.showBalance();
 
-//        Shape square = new Square(5);
-//        square.draw();
-//        System.out.println(square.calculateArea());
+    // Shape square = new Square(5);
+    // square.draw();
+    // System.out.println(square.calculateArea());
 
-//        Manager man = new Manager("Pratap", "manager", "Lisbon", 3000);
-//        man.bonuses();
-//        man.managingProjects();
-//        Programmer pro = new Programmer("jonaphael", "senior programmer", "tallin", 6000);
-//        pro.bonuses();
-//        man.performanceReports(pro, 4);
+    // Manager man = new Manager("Pratap", "manager", "Lisbon", 3000);
+    // man.bonuses();
+    // man.managingProjects();
+    // Programmer pro = new Programmer("jonaphael", "senior programmer", "tallin", 6000);
+    // pro.bonuses();
+    // man.performanceReports(pro, 4);
 
-        RailwayReservation rev = new RailwayReservation("laura", 1255, 12.05);
-        rev.selectSeat(22);
-        System.out.println(rev.reservationStatus());
-        ResortReservation revR = new ResortReservation("laura", 222, 12.08);
-        revR.setRoomNumber(12);
-        System.out.println(revR.reservationStatus());
+
+
+    // User user = new User();
+    // user.setInfo();
+    // user.getInfo();
+
+    // String name;
+    // String gender;
+    // int age;
+    // Scanner scanner = new Scanner(System.in);
+    // User[] todosOsUsuarios = new User[5];
+
+    // for(int i = 0; i < 5; i++) {
+    //   System.out.print("Whats your name:");
+    //   name = scanner.nextLine();
+    //   System.out.print("How old are you?: ");
+    //   age = scanner.nextInt();
+    //   scanner.nextLine();
+    //   System.out.print("Whats your gender");
+    //   gender = scanner.nextLine();
+    //   User u = new User(name, age, gender);
+    //   todosOsUsuarios[i] = u;
+    // }
+
+    // for (User user: todosOsUsuarios) {
+    //   user.getInfo();
+    // }
+
+    // scanner.close();
+    // Pet dog = new Dog("hope", "maltipoo", 2);
+    // dog.calculateAgeInHumanYears();
+    // dog.favoriteToy("ball");
+    // Pet bird = new Bird("catita", "corvo", 1);
+    // bird.favoriteToy("barbie");
+    // bird.calculateAgeInHumanYears();
+    // GymMembership normal = new GymMembership("laura", 4);
+    // normal.membershipFee();
+    // System.out.println(normal.getDuration());
+    // System.out.println(normal.getType());
+    // GymMembership premium = new PremiumMembership("laura", 3);
+    // premium.membershipFee();
+    // System.out.println(premium.getType());
     }
 }
